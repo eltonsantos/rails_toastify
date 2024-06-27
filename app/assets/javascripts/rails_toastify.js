08f7addc954a13626a2eff6e302bb8c54dd7fd95
Elton Santos <eltonaxl@hotmail.com>
2024-06-27 00:52:10 -0300

    Change css and js to add more effects and styles

--- app/assets/javascripts/rails_toastify.js
+++ app/assets/javascripts/rails_toastify.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   window.RailsToastify = {
-    showToast(message, type = 'info', duration = 3000) {
+    showToast(message, type = 'info', duration = 5000) {
       const toastContainer = document.querySelector('.toast-container') || createToastContainer();
-      const toast = createToast(message, type);
+      const toast = createToast(message, type, duration);
       toastContainer.appendChild(toast);
       setTimeout(() => {
         toast.classList.add('fade-out');
@@ -18,10 +18,39 @@ document.addEventListener('DOMContentLoaded', () => {
     return container;
   }
 
-  function createToast(message, type) {
+  function createToast(message, type, duration) {
     const toast = document.createElement('div');
     toast.className = `toast toast--${type}`;
-    toast.textContent = message;
+    toast.classList.add('show');
+  
+    const closeButton = document.createElement('button');
+    closeButton.className = 'toast__close-button';
+    closeButton.innerHTML = '<svg aria-hidden="true" viewBox="0 0 14 16"><path fill-rule="evenodd" d="M7.71 8.23l3.75 3.75-1.48 1.48-3.75-3.75-3.75 3.75L1 11.98l3.75-3.75L1 4.48 2.48 3l3.75 3.75L9.98 3l1.48 1.48-3.75 3.75z"></path></svg>';
+    closeButton.addEventListener('click', () => {
+      toast.classList.add('hide');
+      toast.addEventListener('transitionend', () => toast.remove());
+    });
+  
+    const progressBar = document.createElement('div');
+    progressBar.className = `toast__progress-bar toast__progress-bar--${type}`;
+    progressBar.style.animationDuration = `${duration}ms`;
+  
+    const body = document.createElement('div');
+    body.className = 'toast__body';
+    body.textContent = message;
+  
+    toast.appendChild(body);
+    toast.appendChild(closeButton);
+    toast.appendChild(progressBar);
+  
+    toast.addEventListener('click', () => {
+      toast.classList.add('hide');
+      toast.addEventListener('transitionend', () => toast.remove());
+    });
+    toast.addEventListener('mouseover', () => progressBar.style.animationPlayState = 'paused');
+    toast.addEventListener('mouseout', () => progressBar.style.animationPlayState = 'running');
+  
     return toast;
   }
+  
 });
